5b4b505f839652133fa3b1e7fcc4ae36a6cc6db5
Jado0o0o <harshchaudhary9560031723@gmail.com>
2022-07-07 13:23:31 +0530

    updated to day 5 intern

--- app.js
+++ app.js
@@ -73,6 +73,36 @@ $('#tf-3').hover(function () {
 
 
 
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+        body.classList.remove("scroll-up");
+        return;
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up");
+        body.classList.add("scroll-down");
+    } else if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down");
+        body.classList.add("scroll-up");
+    }
+    lastScroll = currentScroll;
+});
+
+
+
+
+
+
+
+
+
+
+
 qaItems = document.querySelectorAll('.QA-item');
 
 qaItems.forEach((qaItem) => {
